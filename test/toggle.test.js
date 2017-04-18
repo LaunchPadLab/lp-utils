74bd4cf50ffb69a8a1a485ffa10e3f3d473d458d
David Pickart <davidstoreypickart@gmail.com>
2017-04-18 11:49:40 -0500

    Add toggle setter and test

--- test/toggle.test.js
+++ test/toggle.test.js
@@ -1,16 +1,33 @@
 import React, { PropTypes } from 'react'
 import { shallow } from 'enzyme'
-import compose from 'lodash/fp/compose'
 import { toggle, togglePropTypes } from '../src/'
 
-test('toggle provides a toggle value and function', () => {
+
+test('toggle provides a toggle value and setter function', () => {
 
   const Wrapped = () => <h1>Hi</h1>
+  const Wrapper = toggle('test')(Wrapped)
+  const component = shallow(<Wrapper/>)
 
-  const Wrapper = compose(
-    toggle('test')
-  )(Wrapped)
+  // Check props
+  const { testActive, setTest } = component.props()
+  expect(testActive).toBe(false)
+  expect(typeof setTest).toBe('function')
 
+  // Call setter
+  setTest(true)
+  expect(component.props().testActive).toBe(true)
+  setTest(false)
+  expect(component.props().testActive).toBe(false)
+
+  // Don't allow non-bool values
+  expect(() => setTest('string')).toThrow()
+})
+
+test('toggle provides a toggle value and toggle function', () => {
+
+  const Wrapped = () => <h1>Hi</h1>
+  const Wrapper = toggle('test')(Wrapped)
   const component = shallow(<Wrapper/>)
 
   // Check props
@@ -28,8 +45,10 @@ test('toggle provides a toggle value and function', () => {
 test('toggleProptypes creates proptypes with the correct name and value', () => {
   const expectedPropTypes = {
     testActive: PropTypes.bool,
+    setTest: PropTypes.func,
     toggleTest: PropTypes.func,
     test2Active: PropTypes.bool,
+    setTest2: PropTypes.func,
     toggleTest2: PropTypes.func,
   }
   expect(togglePropTypes('test', 'test2')).toEqual(expectedPropTypes)
