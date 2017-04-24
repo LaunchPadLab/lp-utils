d0aeeb2ef67c7473db642c3003c90c3c947bd767
David Pickart <davidstoreypickart@gmail.com>
2017-04-24 14:40:46 -0500

    Upgrade React and add precommit hook

--- test/toggle.test.js
+++ test/toggle.test.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { shallow } from 'enzyme'
 import { toggle, togglePropTypes } from '../src/'
 
