e65f7bab245ed9619a6c1bbf0f488d067a9738ef
David Pickart <davidstoreypickart@gmail.com>
2017-07-11 11:52:23 -0500

    Add componentWithClass function

---
+++ src/component-with-class.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+/**
+ * A function that adds a default className to a React component.
+ * This className will be extended, not overridden, by any additional classNames.
+ * 
+ * @name componentWithClass
+ * @type Function
+ * @param {Function|String} component - The React component that will receive the default class
+ * @param {String} defaultClass - The default class to add to the component
+ *
+ * @example
+ *
+ * const Block = componentWithClass('section', 'section-block')
+ * const Header = componentWithClass('div', 'section-header')
+ *
+ * function Content () {
+ *   return (
+ *     <Block>
+ *       <Header className="highlighted">
+ *         This is some header text!
+ *       </Header>
+ *     </Block>
+ *   )
+ * }
+ * 
+ * // This is equivalent to:
+ * // function Content () {
+ * //   return (
+ * //     <section className="section-block">
+ * //       <div className="section-header highlighted">
+ * //         This is some header text!
+ * //       </div>
+ * //     </section>
+ * //   )
+ * // }
+ *
+ */
+
+function componentWithClass (WrappedComponent, defaultClass) {
+  if (!defaultClass) throw 'You must provide a default class to componentWithClass()'
+  function ClassWrapper ({ children, className, ...rest }) {
+    return (
+      <WrappedComponent 
+        className={ classnames(defaultClass, className) }
+        { ...rest }
+      >
+        { children }
+      </WrappedComponent>
+    )
+  }
+  ClassWrapper.propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string.isRequired,
+  }
+  return ClassWrapper
+}
+
+export default componentWithClass
