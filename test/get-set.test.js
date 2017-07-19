4c9d8b01903f5592e661ad7405cc14ca8639c52d
David Pickart <davidstoreypickart@gmail.com>
2017-07-19 14:58:46 -0500

    Allow falsey initial values for getSet

--- test/get-set.test.js
+++ test/get-set.test.js
@@ -72,6 +72,18 @@ test('getSet can set initial values from props', () => {
 
 })
 
+test('getSet can receive falsey initial values', () => {
+
+  const Wrapped = () => <h1>Hi</h1>
+  const Wrapper = getSet('foo')(Wrapped)
+  const component = shallow(<Wrapper initialValues={ { foo: false } } />)
+
+  // Check props
+  const { foo } = component.props()
+  expect(foo).toBe(false)
+
+})
+
 test('getSetPropTypes creates proptypes with the correct name and value', () => {
   const expectedPropTypes = {
     test: PropTypes.any,
